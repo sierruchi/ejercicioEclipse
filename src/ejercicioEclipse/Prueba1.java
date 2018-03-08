ae4f2929ddfc0fd9647220fc86834c95f56e400e
Nacho Soto <isoto@viewnext.com>
2018-03-08 17:09:31 +0100

    commit 1

---
+++ src/ejercicioEclipse/Prueba1.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package ejercicioEclipse;
+
+/**
+ * @author default
+ *
+ */
+public class Prueba1 {
+
+	/**
+	 * 
+	 */
+	public Prueba1() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hola");
+
+	}
+
+}
